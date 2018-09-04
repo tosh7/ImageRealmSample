1db128d8c50b11f1541dc9cc39e75fc37af56752
tosh7 <zlia.6.lj.425@gmail.com>
2018-09-05 01:41:00 +0900

    made a photoSystem

--- ImageRealmSample/Sources/Controllers/TakeAPhotoViewController.swift
+++ ImageRealmSample/Sources/Controllers/TakeAPhotoViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class TakeAPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    var flag = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        useCamera()
+        print(photoImage)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        useCamera()
+        
+        if flag{
+            flag = false
+            useCamera()
+        }
+    }
+    
+    @IBAction func retakePhoto(_ sender: Any) {
+        self.useCamera()
+    }
+    
+    @IBAction func savePhoto(_ sender: Any) {
     }
     
     func useCamera() {
@@ -33,5 +47,11 @@ class TakeAPhotoViewController: UIViewController, UIImagePickerControllerDelegat
         } else {
             print("error")
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoImage.image = info[UIImagePickerControllerEditedImage ] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
     }
 }
