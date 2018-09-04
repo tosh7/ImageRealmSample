29c4bc60a53e8c9439a010c2f6a9d9f20344b173
Satoshi Komatsu <satoshikomatsu@SatoshinoiMac.local>
2018-09-04 19:08:59 +0900

    made a base system

---
+++ ImageRealmSample/Sources/Controllers/AlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumViewController.swift
+//  ImageRealmSample
+//
+//  Created by Satoshi Komatsu on 2018/09/04.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
---
+++ ImageRealmSample/Sources/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRealmSample
+//
+//  Created by Satoshi Komatsu on 2018/08/30.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
